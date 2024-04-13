a9d1d3251db8ba4f47f1080d957bcd1683b194a5
Sergio D. Barrera <sdbeng@gmail.com>
2024-04-13 14:03:06 -0700

    Add search by name tests

--- src/test/java/com/example/demo/controllers/ItemControllerTest.java
+++ src/test/java/com/example/demo/controllers/ItemControllerTest.java
@@ -73,4 +73,28 @@ public class ItemControllerTest {
         assertNotNull(response);
         assertEquals(404, response.getStatusCodeValue());
     }
+
+    @Test
+    public void search_by_name_error_path() {
+        String name = "Round gadget";
+        when(itemRepository.findByName(name)).thenReturn(new ArrayList<>());
+
+        ResponseEntity response = itemController.getItemsByName(name);
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void search_by_name_happy_path() {
+        String name = "Round gadget";
+        ArrayList<Item> items = new ArrayList<>();
+        Item item = new Item();
+        item.setName("Round gadget");
+        items.add(item);
+        when(itemRepository.findByName(name)).thenReturn(items);
+
+        ResponseEntity response = itemController.getItemsByName(name);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+    }
 }
