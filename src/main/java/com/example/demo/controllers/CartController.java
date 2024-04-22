07eef96153e0a9715eb3c2f9dcfa930d188677eb
Sergio D. Barrera <sdbeng@gmail.com>
2024-04-22 13:13:26 -0700

    add log4j, create instance, capture logs in cart controller

--- src/main/java/com/example/demo/controllers/CartController.java
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -5,6 +5,8 @@ import java.util.stream.IntStream;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
+import com.example.demo.Logger.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +26,8 @@ import com.example.demo.model.requests.ModifyCartRequest;
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
-
-//	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	private Logger Logger;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -35,30 +37,34 @@ public class CartController {
 	
 	@Autowired
 	private ItemRepository itemRepository;
+
+	private static final org.apache.logging.log4j.Logger log = LogManager.getLogger(CartController.class);
 	
 	@PostMapping("/addToCart")
 	public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
-//		log.info("CartService: Add to cart service started...");
+		log.info("CartService: Add to cart service started...");
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
+			Logger.logToCsv(null,"Service: addToCart", "Invalid username -- unable to add item", "404");
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		Optional<Item> item = itemRepository.findById(request.getItemId());
 		if(!item.isPresent()) {
+			Logger.logToCsv(null,"Service: addToCart", "Invalid itemid -- unable to add item", "404");
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		Cart cart = user.getCart();
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.addItem(item.get()));
-//        log.info("Add to Cart: item(s) added to cart successfully...{}", cart.getItems().size());
+        Logger.logToCsv(user.getId(),"Service: addToCart", "Item added successfully ", "200");
 		cartRepository.save(cart);
-//		log.info("Add to Cart: cart saved successfully...done");
+		log.info("Add to Cart: cart saved successfully...done");
 		return ResponseEntity.ok(cart);
 	}
 	
 	@PostMapping("/removeFromCart")
 	public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
-//		log.info("CartService: Remove from cart service started...");
+		log.info("CartService: Remove from cart service started...");
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -71,7 +77,7 @@ public class CartController {
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.removeItem(item.get()));
 		cartRepository.save(cart);
-//        log.info("Remove from Cart: item(s) removed from cart successfully...{}", cart.getItems().size());
+        log.info("Remove from Cart: item(s) removed from cart successfully...{}", cart.getItems().size());
 		return ResponseEntity.ok(cart);
 	}
 		
